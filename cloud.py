a697f98332344cd90a8f8d7399ad2f54787fc6d9
hpeind <balaprati@gmail.com>
2021-08-10 14:19:31 +0530

    set debug to false

--- cloud.py
+++ cloud.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import os
 app=Flask(__name__)
 @app.route("/")
 def home():
     return "Hello Flask"
 if __name__=="__main__":
-    app.run(debug=True)
+    port=os.environ("PORT",5000)
+    app.run(debug=False, host="0,0,0,0", port=port)
